04f395a882866b0dd95c2de402abd6bbdf86a90c
Jesse Bennett <jesse.alexander.bennett@gmail.com>
2019-01-10 14:57:34 -0500

    Console App in Mono

--- RunHost/Program.cs
+++ RunHost/Program.cs
@@ -1,4 +1,12 @@
 ﻿using System;
+using System.IO;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Builder.Extensions;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Hosting;
+using SiteHost;
+
 
 namespace RunHost
 {
@@ -6,7 +14,35 @@ namespace RunHost
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting Server!");
+
+            var webhost = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5433")
+                .Build();
+
+            webhost.Run();
+
+            Console.ReadLine();
+
+
+        }
+
+        public class Startup
+        {
+            public void Configure(IApplicationBuilder app)
+            {
+
+                var xmlName = "Doesnt.Matter.xml";
+                var xmlPath = Path.Combine(Path.GetDirectoryName(Assembly.GetEntryAssembly().Location), xmlName);
+
+                app.UseMagicOnionSwagger(new SwaggerOptions("SiteHost.Site", "Site Test", "/")
+                {
+                    XmlDocumentPath = xmlPath
+                });
+            }
         }
     }
+
 }
